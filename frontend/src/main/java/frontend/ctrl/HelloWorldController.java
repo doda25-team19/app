8e9dbfe6af1393fb6e976cce8e7ec705f932f25d
Miroslav Atanasov <m.d.atanasov@student.tudelft.nl>
2026-01-18 10:23:20 +0100

    add version info in reposponce for testing

--- frontend/src/main/java/frontend/ctrl/HelloWorldController.java
+++ frontend/src/main/java/frontend/ctrl/HelloWorldController.java
@@ -1,5 +1,6 @@
 package frontend.ctrl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloWorldController {
 
+    @Value("${APP_VERSION:unknown}")
+    private String appVersion;
+
     @GetMapping("/")
     @ResponseBody
     public String index() {
-        return "Hello World!";
+       return String.format("Hello World! [Version: %s]", appVersion);
     }
 }
